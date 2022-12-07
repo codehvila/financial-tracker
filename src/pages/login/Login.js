dc98ad37937d2105dc6258173c29ac9be21d51b3
Hugo Vila <code.hvila@gmail.com>
2022-12-07 19:08:10 +0100

    Basic pages structure

---
+++ src/pages/login/Login.js
@@ -0,0 +1,5 @@
+import styles from "./Login.module.css";
+
+export default function Login() {
+  return <div>Login</div>;
+}
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,5 @@
+import styles from "./Signup.module.css";
+
+export default function Signup() {
+  return <div>Signup</div>;
+}
