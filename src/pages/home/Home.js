2633ae72d288722c1a09febfe23a707447ac798c
Hugo Vila <code.hvila@gmail.com>
2022-12-12 14:03:10 +0100

    Emphasize home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,10 @@
 import styles from "./Home.module.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>Home</div>
+      <div className={styles.Principal}>Finance Tracker</div>
+    </div>
+  );
 }
