29aa41082e3bf24c799ea03e3099025760cfd449
Hugo Vila <code.hvila@gmail.com>
2022-12-13 13:56:07 +0100

    Create Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
 import styles from "./Login.module.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <form onSubmit={handleLogin} className={styles["login-form"]}>
+        <h1>Login</h1>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
