45937c22544bffee4b9f7a624facc144588a6afe
Hugo Vila <code.hvila@gmail.com>
2022-12-24 14:29:56 +0100

    Show login, signup, user logout conditionally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,37 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>
           <Link to="/">Finance Tracker</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button onClick={logout} className="btn">
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>{user.displayName}</li>
+            <li>
+              <button onClick={logout} className="btn">
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
