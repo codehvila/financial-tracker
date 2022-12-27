d9cfaa9376aeeeb3d485d0c7c8edd419669008e1
Hugo Vila <code.hvila@gmail.com>
2022-12-27 18:05:16 +0100

    Setup Route Guarding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -7,16 +7,19 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
       {authIsReady && (
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={!user ? <Login /> : <Home />} />
+            {/* <Route path="/" element={!user ? <Navigate replace={true} to="/login" /> : <Home />} /> */}
+            <Route path="/login" element={user ? <Home /> : <Login />} />
+            {/* <Route path="/login" element={user ? <Navigate replace={true} to="/" /> : <Login />} /> */}
+            <Route path="/signup" element={user ? <Home /> : <Signup />} />
+            {/* <Route path="/signup" element={user ? <Navigate replace={true} to="/" /> : <Signup />} /> */}
           </Routes>
         </BrowserRouter>
       )}
