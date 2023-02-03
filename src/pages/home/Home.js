4ccb36b16e83e355cb02d93d23c96911d929e260
Hugo Vila <code.hvila@gmail.com>
2023-02-03 17:33:53 +0100

    Update Home page to use Transaction List component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,23 @@
-import TransactionForm from "./TransactionForm";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 import styles from "./Home.module.css";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions");
+
   return (
     <>
       <div className={styles.title}>Home Finance Tracker</div>
       <div className={styles.container}>
-        <div className={styles.content}>transaction list</div>
+        <div className={styles.content}>
+          <h3>transaction list</h3>
+
+          {error && <p>error</p>}
+          {documents && <TransactionList transactions={documents} />}
+        </div>
         <div className={styles.sidebar}>
           <TransactionForm uid={user} />
         </div>
