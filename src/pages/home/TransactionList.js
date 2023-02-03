61ed76f9cc3375db3e2c3105c36b8bb8287c824e
Hugo Vila <code.hvila@gmail.com>
2023-02-03 17:32:20 +0100

    Create a component to list transactions

---
+++ src/pages/home/TransactionList.js
@@ -0,0 +1,20 @@
+import styles from "./Home.module.css";
+
+export default function TransactionList({ transactions }) {
+  return (
+    <ul className={styles.transactions}>
+      {transactions &&
+        transactions.map((doc) => (
+          <li key={doc.id}>
+            <p className={styles.date}>
+              {new Intl.DateTimeFormat("es-ES").format(
+                new Date(doc.createdAt.toDate())
+              )}
+            </p>
+            <p className={styles.name}>{doc.name}</p>
+            <p className={styles.amount}>{doc.amount}$</p>
+          </li>
+        ))}
+    </ul>
+  );
+}
