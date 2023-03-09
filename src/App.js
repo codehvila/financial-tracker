9f21e2e2d232c2f89d8ef2e65b811b711fd57501
Hugo Vila <code.hvila@gmail.com>
2023-03-09 18:37:03 +0100

    fix: Remove unused methode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
